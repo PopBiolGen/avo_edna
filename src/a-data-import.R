62abe11fc7f3c19aba0aa95d31910c03cb9249f4
Ben Phillips <ben.l.phillips@curtin.edu.au>
2024-09-13 11:05:49 +0800

    Add data import script

---
+++ src/a-data-import.R
@@ -0,0 +1,8 @@
+
+data.path <- c("dat/Occupancy")
+
+# species data
+sp.df <- read.csv(file = file.path(data.path, "eDNA_all_species.txt"), sep = "\t")
+
+# environment data (trip = season; trip 2 is peak)
+env.df <- read.csv(file = file.path(data.path, "eDNA_env.txt"), sep = "\t")
